2b944bc0d7bb32a620666c227deab2a85fac1295
Allan Hans <allan-boop>
2024-02-19 17:34:40 +0100

    end of day

--- src/core_logic.c
+++ src/core_logic.c
@@ -6,7 +6,7 @@
 /*   By: ahans <ahans@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 14:32:56 by ahans             #+#    #+#             */
-/*   Updated: 2024/02/19 17:08:20 by ahans            ###   ########.fr       */
+/*   Updated: 2024/02/19 17:32:56 by ahans            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 int	eat(t_philo *philo)
 {
-	philo = philo;
-	//philo->time_of_eat--;
+	philo->time_of_eat--;
 	return (0);
 }
 
-void	*philo_life(void *philo)
+void	*philo_life(t_philo *philo)
 {
-	//while (philo->time_of_eat > 0)
-	eat(philo);
+	while (philo->time_of_eat > 0)
+		eat(philo);
 	return (NULL);
 }
 
@@ -36,7 +35,7 @@ int	execute_core_logic(t_params params)
 		return (-1);
 	while (philo_nb--)
 	{
-		if (pthread_create(&params.philos[philo_nb].thread, NULL, \
+		if (pthread_create(&params.philos[philo_nb].thread, NULL,
 		(void *)philo_life, &params.philos[philo_nb]) != 0)
 			return (ft_error(ERR_PTHREAD));
 	}
--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: ahans <ahans@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 11:54:01 by ahans             #+#    #+#             */
-/*   Updated: 2024/02/19 16:19:24 by ahans            ###   ########.fr       */
+/*   Updated: 2024/02/19 17:26:38 by ahans            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	free_tab(t_params params, int i)
 {
 	int	j;
 
+	params = params;
 	j = 0;
 	while (j < i)
 	{
