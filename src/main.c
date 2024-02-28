451a01775d052f7abf3256713f6caea6450c1c29
Allan Hans <allan-boop>
2024-02-28 14:08:20 +0100

    bug resolve

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ahans <ahans@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 15:39:08 by ahans             #+#    #+#             */
-/*   Updated: 2024/02/28 13:00:39 by ahans            ###   ########.fr       */
+/*   Updated: 2024/02/28 14:07:38 by ahans            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ int	main(int ac, char **av)
 		return (-1);
 	if (philos[0].params->nb_of_philo == 1)
 	{
-		printf("0 1 has taken a fork\n%d 1 died\n",
-			philos[0].params->time_to_die); //usleep temps de vie
+		printf("0 1 has taken a fork\n");
+		usleep(philos[0].params->time_to_die * 1000);
+		printf("%d 1 died\n", philos[0].params->time_to_die);
 	}
 	else
 		execute_core_logic(philos);
